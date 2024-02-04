c34665450c0b5c89ef675021c9a1e82376b713c9
DiegoZM1006 <1109184718@u.icesi.edu.co>
2024-02-04 16:33:01 -0500

    refactor: Extract common code to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,41 @@ export const USDollar = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
 });
+
+export function categoryTranslate(str) {
+    const dictionary = {
+        kids: "Niños",
+        man: "Hombre",
+        women: "Mujer"
+    }
+    return dictionary[str];
+}
+
+export function typeTranslate(str) {
+    const dictionary = {
+        shoes: "Zapatos",
+        clothes: "Ropa",
+        accesories: "Accesorios"
+    }
+    return dictionary[str];
+}
+
+export function categoryProductsArr(str) {
+    const dictionary = {
+        Niños: [
+            { title: "Zapatos", titleEn: "shoes", url: "/kid-shoes.webp" },
+            { title: "Ropa", titleEn: "clothes", url: "/kid-clothes.webp" },
+          ],
+          Hombre: [
+            { title: "Zapatos", titleEn: "shoes", url: "/man-shoes.webp" },
+            { title: "Ropa", titleEn: "clothes", url: "/man-clothes.webp" },
+            { title: "Accesorios", titleEn: "accesories", url: "/man-accesories.webp" },
+          ],
+          Mujer: [
+            { title: "Zapatos", titleEn: "shoes", url: "/women-shoes.webp" },
+            { title: "Ropa", titleEn: "clothes", url: "/women-clothes.webp" },
+            { title: "Accesorios", titleEn: "accesories", url: "/women-accesories.webp" },
+          ],
+    }
+    return dictionary[str];
+};
